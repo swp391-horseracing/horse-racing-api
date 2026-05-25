42469e53b42abf8571ea67caf8f2ab9b59c21878
patohru <patohru2006@gmail.com>
2026-05-25 14:01:29 +0700

    feat: add auto generate uuid

--- src/schema/user.js
+++ src/schema/user.js
@@ -18,8 +18,8 @@ export const rolesEnum = pgEnum("roles", [
 export const statusEnums = pgEnum("status", ["pending", "active", "locked"]);
 
 export const users = pgTable("users", {
-    id: uuid("id").primaryKey(),
-    full_name: varchar("name", { length: 255 }).notNull(),
+    id: uuid("id").defaultRandom().primaryKey(),
+    full_name: varchar("full_name", { length: 255 }).notNull(),
     email: varchar("email", { length: 255 }).notNull().unique(),
     password: text("password").notNull(),
     phone: varchar("phone", { length: 15 }).notNull(),
