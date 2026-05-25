42fdb6118aecfd386c2dc7a2c7df812e836ac5d1
patohru <patohru2006@gmail.com>
2026-05-25 23:54:30 +0700

    refactor: convert to ts and handle service unavaiable error

--- src/middleware/error.js
+++
@@ -1,6 +0,0 @@
-export const errorMiddleware = (err, _req, res) => {
-    console.error(err.cause);
-    res.status(err.status || 500).json({
-        error: err.cause || "Internal Server Error",
-    });
-};
---
+++ src/middleware/error.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+
+export const errorMiddleware = (
+    err: any,
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    console.error(err);
+
+    if (err.cause.code === "ENOTFOUND") {
+        return res.status(503).json({
+            error: {
+                code: "SERVICE_UNAVAILABLE",
+                message: "Unable to connect to the database",
+            },
+        });
+    }
+
+    return res.status(500).json({
+        error: err.code || "Internal Server Error",
+    });
+};
