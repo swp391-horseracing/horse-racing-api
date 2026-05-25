35ac117097428736dec9d1c17d1b8aee72ca7824
patohru <patohru2006@gmail.com>
2026-05-25 20:45:23 +0700

    chore: fix lint

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,4 +1,4 @@
-export const errorMiddleware = (err, _req, res, _next) => {
+export const errorMiddleware = (err, _req, res) => {
     console.error(err.cause);
     res.status(err.status || 500).json({
         error: err.cause || "Internal Server Error",
