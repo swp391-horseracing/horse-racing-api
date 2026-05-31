bcba0ce93a764d975f4b8630c918c2d5c255b5fe
patohru <patohru2006@gmail.com>
2026-05-31 21:18:01 +0700

    build: seprate generate and migrage for drizzle script

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import express from "express";
 import morgan from "morgan";
 import { errorMiddleware } from "./middleware/error.js";
 import router from "./route/index.js";
+import { generateOpenApiDoc } from "./docs/openapi.js";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(morgan("dev"));
 app.use(express.json());
 
+const openApiDoc = generateOpenApiDoc();
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openApiDoc));
 app.use("/api", router);
 
 app.use(errorMiddleware);
