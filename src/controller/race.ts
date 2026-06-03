b55b817357edb6e00e005a1977498959a10fcaac
patohru <patohru2006@gmail.com>
2026-06-03 20:26:03 +0700

    feat: add uuid validation

--- src/controller/race.ts
+++ src/controller/race.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import db from "../config/db.js";
 import { races } from "../schema/races.js";
+import { validate as uuidValidate } from "uuid";
 import { eq, ne, and } from "drizzle-orm";
 
 export const getRace = async (
@@ -10,6 +11,10 @@ export const getRace = async (
 ) => {
     try {
         const raceId = req.params.raceId as string;
+        console.log(raceId);
+        if (!uuidValidate(raceId)) {
+            return res.status(400).json({ message: "Invalid uuid" });
+        }
 
         const [race] = await db
             .select()
@@ -17,7 +22,7 @@ export const getRace = async (
             .where(and(eq(races.id, raceId), ne(races.status, "draft")));
 
         if (!race) {
-            return res.json({ message: "Race not exist" });
+            return res.status(404).json({ message: "Race not exist" });
         }
 
         res.json(race);
