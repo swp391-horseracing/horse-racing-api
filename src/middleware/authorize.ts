86bd3f828edf5710c31a778ad121816606e9eae5
patohru <patohru2006@gmail.com>
2026-06-04 09:57:44 +0700

    feat: separate http error to req.user and req.user.role

--- src/middleware/authorize.ts
+++ src/middleware/authorize.ts
@@ -3,7 +3,11 @@ import { Role } from "../types/roles.js";
 
 export function authorize(...roles: Role[]) {
     return (req: Request, res: Response, next: NextFunction) => {
-        if (!req.user || !roles.includes(req.user.role)) {
+        if (!req.user) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        if (!roles.includes(req.user.role)) {
             return res.status(403).json({ message: "Forbidden" });
         }
         next();
