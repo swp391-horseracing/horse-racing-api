da782309c65b2abcebe70dc51cf6765003baab4e
patohru <patohru2006@gmail.com>
2026-06-07 11:32:33 +0700

    feat: add openapi.yaml then apply it to app

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,9 @@ import express from "express";
 import morgan from "morgan";
 import { errorMiddleware } from "./middleware/error.js";
 import router from "./route/index.js";
-import { generateOpenApiDoc } from "./docs/openapi.js";
 import swaggerUi from "swagger-ui-express";
+import { readFileSync } from "fs";
+import { parse } from "yaml";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 import db from "./config/db.js";
 import cors from "cors";
@@ -18,8 +19,9 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-const openApiDoc = generateOpenApiDoc();
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(openApiDoc));
+const spec = parse(readFileSync("./openapi.yaml", "utf-8"));
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(spec));
 app.use("/api", router);
 
 app.use(errorMiddleware);
