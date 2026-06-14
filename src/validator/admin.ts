62a5cf0bf7283c4fb7375fa9b1d75d0ae6b126e8
patohru <patohru2006@gmail.com>
2026-06-14 11:04:07 +0700

    feat: add create tournaments validate schema with superRefine to validate datetime

--- src/validator/admin.ts
+++ src/validator/admin.ts
@@ -18,19 +18,60 @@ const updateStatusSchema = z.object({
     status: z.enum(["active", "locked"]),
 });
 
-const createTournamentSchema = z.object({
-    name: z.string().min(3).max(100),
-    startDate: z.coerce.date(),
-    endDate: z.coerce.date(),
-    description: z.string().optional(),
-    rules: z.string().optional(),
-    location: z.string().max(200).optional(),
-    registrationOpenDate: z.iso.datetime().optional(),
-    registrationCloseDate: z.iso.datetime().optional(),
-    maximumParticipants: z.number().int().positive().optional(),
-    minimumParticipants: z.number().int().positive().optional(),
-    prizePool: z.float64().positive().optional(),
-});
+const createTournamentSchema = z
+    .object({
+        name: z.string().min(3).max(100),
+        startDate: z.iso.datetime(),
+        endDate: z.iso.datetime(),
+        description: z.string().optional(),
+        rules: z.string().optional(),
+        location: z.string().max(200).optional(),
+        registrationOpenDate: z.iso.datetime().optional(),
+        registrationCloseDate: z.iso.datetime().optional(),
+        maximumParticipants: z.number().int().positive().optional(),
+        minimumParticipants: z.number().int().positive().optional(),
+        prizePool: z.float64().positive().optional(),
+    })
+    .superRefine((data, ctx) => {
+        const start = new Date(data.startDate);
+        const end = new Date(data.endDate);
+        if (start >= end) {
+            ctx.addIssue({
+                code: "custom",
+                path: ["endDate"],
+                message: "End date must be after start date",
+            });
+        }
+        if (!data.registrationOpenDate || !data.registrationCloseDate) return;
+
+        const regOpen = new Date(data.registrationOpenDate);
+        const regClose = new Date(data.registrationCloseDate);
+
+        if (regOpen >= regClose) {
+            ctx.addIssue({
+                code: "custom",
+                path: ["registrationCloseDate"],
+                message:
+                    "Registrationclose date must be after registration open date",
+            });
+        }
+
+        if (regClose >= start) {
+            ctx.addIssue({
+                code: "custom",
+                path: ["registrationCloseDate"],
+                message: "Registration must close before the tournament starts",
+            });
+        }
+
+        if (regOpen >= regClose) {
+            ctx.addIssue({
+                code: "custom",
+                path: ["registrationCloseDate"],
+                message: "Registration must close after registration open",
+            });
+        }
+    });
 
 export {
     usersQuerySchema,
