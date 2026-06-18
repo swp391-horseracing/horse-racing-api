0a0015dd610104c5b733f27f83d289430630ef13
patohru <patohru2006@gmail.com>
2026-06-18 14:42:51 +0700

    feat: add unique to race id for race result

--- src/schema/raceResults.ts
+++ src/schema/raceResults.ts
@@ -1,4 +1,4 @@
-import { pgTable, timestamp, uuid, pgEnum } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, pgEnum, unique } from "drizzle-orm/pg-core";
 import { races } from "./races.js";
 import { users } from "./users.js";
 
@@ -8,19 +8,27 @@ export const resultStatusEnums = pgEnum("result_status", [
     "published",
 ]);
 
-export const raceResults = pgTable("race_results", {
-    id: uuid("id").defaultRandom().primaryKey(),
-    raceId: uuid("race_id")
-        .references(() => races.id)
-        .notNull(),
-    resultStatus: resultStatusEnums("result_status").default("draft").notNull(),
-    refereeConfirmedBy: uuid("referee_confirmed_by").references(() => users.id),
-    refereeConfirmedAt: timestamp("referee_confirmed_at"),
-    publishedBy: uuid("published_by").references(() => users.id),
-    publishedAt: timestamp("published_at"),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at")
-        .defaultNow()
-        .$onUpdate(() => new Date())
-        .notNull(),
-});
+export const raceResults = pgTable(
+    "race_results",
+    {
+        id: uuid("id").defaultRandom().primaryKey(),
+        raceId: uuid("race_id")
+            .references(() => races.id)
+            .notNull(),
+        resultStatus: resultStatusEnums("result_status")
+            .default("draft")
+            .notNull(),
+        refereeConfirmedBy: uuid("referee_confirmed_by").references(
+            () => users.id,
+        ),
+        refereeConfirmedAt: timestamp("referee_confirmed_at"),
+        publishedBy: uuid("published_by").references(() => users.id),
+        publishedAt: timestamp("published_at"),
+        createdAt: timestamp("created_at").defaultNow().notNull(),
+        updatedAt: timestamp("updated_at")
+            .defaultNow()
+            .$onUpdate(() => new Date())
+            .notNull(),
+    },
+    (table) => [unique().on(table.raceId)],
+);
