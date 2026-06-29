962c34fc6da731f8356b99c3a7ca85b26b19a54f
patohru <patohru2006@gmail.com>
2026-06-30 00:17:27 +0700

    chore: apply result route

--- src/route/race.ts
+++ src/route/race.ts
@@ -4,6 +4,7 @@ import {
     getRaceEntries,
     getHorseEntries,
     getRace,
+    getRaceResult,
 } from "../controller/race.js";
 import { optionalAuthMiddleware } from "../middleware/optionalAuth.js";
 import { authMiddleware } from "../middleware/auth.js";
@@ -15,6 +16,7 @@ const router = Router();
 router.get("/:raceId", optionalAuthMiddleware, getRace);
 router.get("/:raceId/horses", optionalAuthMiddleware, getHorseEntries);
 router.get("/:raceId/entries", optionalAuthMiddleware, getRaceEntries);
+router.get("/:raceId/results", getRaceResult);
 router.post(
     "/:raceId/predictions",
     authMiddleware,
