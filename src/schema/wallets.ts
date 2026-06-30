12e733fe3e5e570c525f76d9224e344d42e4a492
ductpt2 <ductpt2@gmail.com>
2026-06-30 11:25:57 +0700

    feat: add wallet and walletTransaction schema

---
+++ src/schema/wallets.ts
@@ -0,0 +1,30 @@
+import {
+    integer,
+    pgTable,
+    timestamp,
+    uuid,
+} from "drizzle-orm/pg-core";
+import { users } from "./users.js";
+
+export const wallets = pgTable("wallets", {
+    id: uuid("id").defaultRandom().primaryKey(),
+
+    userId: uuid("user_id")
+        .notNull()
+        .unique()
+        .references(() => users.id, {
+            onDelete: "cascade",
+        }),
+
+    balance: integer("balance")
+        .notNull()
+        .default(1000),
+
+    createdAt: timestamp("created_at")
+        .defaultNow()
+        .notNull(),
+
+    updatedAt: timestamp("updated_at")
+        .defaultNow()
+        .notNull(),
+});
