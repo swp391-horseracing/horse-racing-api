6b1e4c0ae0726664aabddc6eaed75b2337c28852
patohru <patohru2006@gmail.com>
2026-07-12 09:55:54 +0700

    fix: drop index if exists

--- drizzle/0015_tiny_baron_zemo.sql
+++ drizzle/0015_tiny_baron_zemo.sql
@@ -1,2 +1,2 @@
-DROP INDEX "uq_referee_race_idx";--> statement-breakpoint
+DROP INDEX IF EXISTS "uq_referee_race_idx";--> statement-breakpoint
 ALTER TABLE "referee_assignments" ADD CONSTRAINT "referee_assignments_race_referee_unique" UNIQUE("race_id","referee_id");
